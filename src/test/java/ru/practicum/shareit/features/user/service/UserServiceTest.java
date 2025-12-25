626a384461d23d4076f932a94d306b4918c70c59
Mike <zybrof@gmail.com>
2025-12-25 12:33:42 +0300

    Tech / Configure test DB

---
+++ src/test/java/ru/practicum/shareit/features/user/service/UserServiceTest.java
@@ -0,0 +1,53 @@
+package ru.practicum.shareit.features.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import ru.practicum.shareit.config.PersistenceConfig;
+import ru.practicum.shareit.factory.UserFactory;
+import ru.practicum.shareit.features.user.model.User;
+import ru.practicum.shareit.features.user.repository.UserRepository;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@ActiveProfiles("test")
+@SpringJUnitConfig(classes = {
+        UserService.class,
+        PersistenceConfig.class,
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+@EntityScan(basePackages = "ru.practicum.shareit")
+public class UserServiceTest {
+    private final UserRepository repository;
+    private UserFactory factory;
+    private final UserService service;
+
+    @Test
+    void testCreateOne() {
+        User creation = getUserFactory().make();
+
+        User createdUser = service.createOne(creation);
+        User fetchedUser = service.getOne(createdUser.getId());
+
+        assertThat(
+                createdUser,
+                equalTo(fetchedUser)
+        );
+    }
+
+    private UserFactory getUserFactory() {
+        if (factory == null) {
+            factory = new UserFactory(repository);
+        }
+
+        return factory;
+    }
+}
